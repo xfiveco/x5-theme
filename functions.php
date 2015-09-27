f0738e7e107bf0a8c7de0ff60a36525f3396f8dc
Maciej Lamberski <maciej@lamberski.com>
2015-09-27 21:43:26 +0200

    Move example usage of theme features to comment above (with brief description)

--- functions.php
+++ functions.php
@@ -4,45 +4,70 @@
  */
 require_once dirname( __FILE__ ) . '/features/load.php';
 
+/**
+ * Creates a nicely formatted and more specific title based on current view.
+ */
 add_theme_support( 'seo-title' );
 
-// add two navigation menus
+/**
+ * Register custom menu locations.
+ *
+ * 'example-menu' => 'Example Menu',
+ */
 add_theme_support( 'menus', array(
-	'navigation-top' => __( 'Top Navigation Menu' ),
-	'navigation-foot' => __( 'Footer Navigation Menu' ),
+
 ) );
 
-// add 3 default sidebars
+/**
+ * Register custom sidebars.
+ *
+ * array(
+ * 	 'name' => 'Example Sidebar',
+ * 	 'id'   => 'example-sidebar',
+ * ),
+ */
 add_theme_support( 'sidebars', array(
-	array(),
-	array(),
-	array(),
+
 ) );
 
+/**
+ * Register custom thumbnail sizes.
+ *
+ * 'example-thumbnail' => array(
+ *   'width'  => 400,
+ *   'height' => 500,
+ *   'crop'   => true,
+ * ),
+ */
 add_theme_support( 'images', array(
-	'400x500' => array(
-	'width' => '400',
-	'height' => '500',
-	'crop' => true,
-	),
+
 ) );
 
+/**
+ * Add custom post types to the theme.
+ *
+ * 'example-post' => array(
+ *   'singular' => 'Example Post',
+ *   'plural'   => 'Example Posts',
+ *   'public'   => true,
+ *   'rewrite'  => array( 'slug' => 'example-post, 'with_front' => true ),
+ *   'supports' => array( 'title', 'editor' ),
+ * ),
+ */
 add_theme_support( 'post-types', array(
-	// team post
-	'wp-light-team' => array(
-	'singular' => 'Team Member',
-	'plural' => 'Team Members',
-	'publicly_queryable' => true,
-	'rewrite' => array( 'slug' => 'team', 'with_front' => true ),
-	),
+
 ) );
 
+/**
+ * Register custom taxonomies (categories) to posts.
+ *
+ * 'example-category' => array(
+ *   'singular' => 'Example Category',
+ *   'plural'   => 'Example Categories',
+ *   'rewrite'  => array( 'slug' => 'example-category', 'with_front' => false ),
+ *   'posts'    => array( 'example-post' ),
+ * ),
+ */
 add_theme_support( 'taxonomies', array(
-	// taxonomy like category
-	'wp-light-team-tag' => array(
-	'singular' => 'Member Category',
-	'plural' => 'Member Categories',
-	'rewrite' => array( 'slug' => 'category', 'with_front' => false ),
-	'posts' => array( 'wp-light-team' ),
-	),
+
 ) );
